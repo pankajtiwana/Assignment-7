3f0e4e65fee6c960683ba05639ca2a6f1a97108b
Mark <Mark@192.168.0.102>
2015-03-02 22:19:17 -0500

    w2015 updates

--- src/cpd3314/assign7/ArrayUtil2D.java
+++ src/cpd3314/assign7/ArrayUtil2D.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2014 Len Payne <len.payne@lambtoncollege.ca>.
+ * Updated 2015 Mark Russell <mark.russell@lambtoncollege.ca>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
